04bea3e5563b70951ca6d1dde87611ff8d31db78
Adame Abdelaziz <adame.abdelaziz@gmail.com>
2024-11-18 22:21:39 +0100

    script make done

--- run_maked.sh
+++ run_maked.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
 
-# Should be used once you are placed in g5k grenoble and created host
+#OAR -q production 
+#OAR -l host=1/gpu=1
+#OAR -l walltime=3:00:00
+#OAR -p gpu-16GB AND gpu_compute_capability_major>=5
+#OAR -O OAR_%jobid%.out
+#OAR -E OAR_%jobid%.err 
 
-# Environment deployment
-
-kadeploy3 debian11-min
-
-# Copy of files
-
-for node in $(uniq "$OAR_NODEFILE"); do   
-    scp ./texte.txt root@"$node":/home/text.txt
-    echo "$node"
-done
+# display some information about attributed resources
+hostname 
+nvidia-smi 
+ 
+# make use of a python torch environment
+module load conda
+conda activate pytorch_env
+python3 -c "import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))";
