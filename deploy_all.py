b0ff3c62250cfe3d744367d5ec5f08fa39ce5f2e
Adame Abdelaziz <adame.abdelaziz@gmail.com>
2024-12-03 17:11:25 +0100

    little changes

--- deploy_all.py
+++ deploy_all.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     site = os.getenv("GRID5000_SITE", "rennes")
     script_path = "./maked/run_maked.sh"
     script_init_path = "./maked/run_make.sh"    
-    directory_list = ["matrix", "premier"]
+    directory_list = ["premier", "matrix"]
     list_int = [2, 3, 4, 6, 8, 11, 16]
 
     for directory in directory_list:
