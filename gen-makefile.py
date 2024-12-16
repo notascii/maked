51037e502e07af83fcdd4883191666f164281755
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-12-16 09:59:40 +0100

    makefiles & graphs

--- gen-makefile.py
+++ gen-makefile.py
@@ -2,17 +2,15 @@ import contextlib
 import sys
 from pathlib import Path
 
-
-file_content = "##########################################################################################"
-
 if __name__ == "__main__":
-    if len(sys.argv) != 7:
+    if len(sys.argv) != 9:
         raise ValueError(
-            "Wrong use of this file. Please use `python gen-makefile.py -n <name> -c <nb_commands> -s <sleep_time>"
+            "Wrong use of this file. Please use `python gen-makefile.py -n <name> -c <nb_commands> -s <sleep_time> -z <file_size>"
         )
     name = None
     commands = None
     sleep = None
+    size = None
     for index, arg in enumerate(sys.argv):
         if arg == "-n":
             name = sys.argv[index + 1]
@@ -20,12 +18,14 @@ if __name__ == "__main__":
             commands = [int(x) for x in sys.argv[index + 1].split(",")]
         elif arg == "-s":
             sleep = float(sys.argv[index + 1])
+        elif arg == "-z":
+            size = int(sys.argv[index + 1])
 
-    if sleep is None or commands is None or name is None:
+    if sleep is None or commands is None or name is None or size is None:
         raise ValueError(
-            "Wrong use of this file. Please use `python gen-makefile.py -n <name> -b <nb_branches> -s <sleep_time>"
+            "Wrong use of this file. Please use `python gen-makefile.py -n <name> -b <nb_branches> -s <sleep_time> -z <file_size>"
         )
-    makefile_path = f"makefiles/{name}-c-{commands}-s-{sleep}"
+    makefile_path = f"makefiles/{name}-c-{commands}-s-{sleep}-z-{size}"
     with contextlib.suppress(FileExistsError):
         Path.mkdir(makefile_path)
 
@@ -41,19 +41,19 @@ if __name__ == "__main__":
     for n0 in range(commands[0]):
         makefile += f"""
 command-{n0}: {" ".join([f"command-{n0}-{nb}" for nb in range(commands[1])])}
-\tsleep {sleep} && echo {file_content} > command-{n0}
+\tsleep {sleep} && echo "{"#" * size}" > command-{n0}
 """
         leaf_commands.append(f"command-{n0}")
         for n1 in range(commands[1]):
             makefile += f"""
 command-{n0}-{n1}: {" ".join([f"command-{n0}-{n1}-{nb}" for nb in range(commands[2])])}
-\tsleep {sleep} && echo {file_content} > command-{n0}-{n1}
+\tsleep {sleep} && echo "{"#" * size}" > command-{n0}-{n1}
 """
             leaf_commands.append(f"command-{n0}-{n1}")
             for n2 in range(commands[2]):
                 makefile += f"""
 command-{n0}-{n1}-{n2}:
-\tsleep {sleep} && echo {file_content} > command-{n0}-{n1}-{n2}
+\tsleep {sleep} && echo "{"#" * size}" > command-{n0}-{n1}-{n2}
 """
                 leaf_commands.append(f"command-{n0}-{n1}-{n2}")
 
@@ -64,3 +64,5 @@ clean:
 
     with open(Path(makefile_path) / "Makefile", "w") as f:
         f.write(makefile)
+    with open(Path(makefile_path) / ".gitignore", "w") as f:
+        f.write("command-*")
